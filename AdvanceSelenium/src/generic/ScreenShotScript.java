9cc3dce769bb94ecde745767449335336999dda6
QSPR <QSPR@DESKTOP-AOFC2TM>
2023-02-07 14:42:55 +0530

    123

--- AdvanceSelenium/src/generic/ScreenShotScript.java
+++ AdvanceSelenium/src/generic/ScreenShotScript.java
@@ -13,7 +13,7 @@ public class ScreenShotScript {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.gecko.driver", "./softwares/geckodriver.exe");
+	.setProperty("webdriver.gecko.driver", "./softwares/geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://www.instagram.com");
 		//step 1
