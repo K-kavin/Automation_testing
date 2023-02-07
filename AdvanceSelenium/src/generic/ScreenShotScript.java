8f4d73bfd003c0e8b6709365c18dd6a85df4994e
QSPR <QSPR@DESKTOP-AOFC2TM>
2023-02-07 14:00:08 +0530

    Build no 201

---
+++ AdvanceSelenium/src/generic/ScreenShotScript.java
@@ -0,0 +1,34 @@
+package generic;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class ScreenShotScript {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.gecko.driver", "./softwares/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.instagram.com");
+		//step 1
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		//step 2
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		//step 3
+		File dst=new File("./screenshot/ .jpeg");
+		try {
+			FileHandler.copy(src, dst);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
